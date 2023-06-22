be3da053913b713889c8f658af550eedbb97b3ef
nodwon <nodwon5024@gmail.com>
2023-06-22 14:51:22 +0900

    #17 springboot configuration proccesmsor 디펜던시 추가 thymeleaf 4 decoupled logic 설정 게시글 detail

---
+++ src/main/java/com/example/board/config/ThymeleafConfig.java
@@ -0,0 +1,47 @@
+package com.example.board.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public Thymeleaf4Properties thymeleaf4Properties() {
+        return new Thymeleaf4Properties();
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf4Properties thymeleaf4Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf4Properties.decoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    @Getter
+    @RequiredArgsConstructor
+    @ConfigurationProperties("spring.thymeleaf4")
+    public static class Thymeleaf4Properties {
+
+        /**
+         * Use Thymeleaf 4 Decoupled Logic
+         */
+
+        public boolean decoupledLogic() {
+            return false;
+        }
+
+    }
+
+
+
+}
--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,9 +14,15 @@ import java.util.List;
 public class ArticleController {
 
     @GetMapping
-    public String articles(ModelMap model) {
-        model.addAttribute("articles", List.of());
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 }
