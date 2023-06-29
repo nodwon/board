e99e3777219ac59a30f922cb2bb3d39e32fd2974
nodwon <nodwon5024@gmail.com>
2023-06-29 16:52:53 +0900

    a

---
+++ src/main/java/com/example/board/dto/response/ArticleResponse.java
@@ -0,0 +1,45 @@
+package com.example.board.dto.response;
+
+import com.example.board.dto.ArticleDto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record ArticleResponse(
+        Long id,
+        String title,
+        String content,
+        String hashtags,
+        //Set<String> hashtags,
+        LocalDateTime createdAt,
+        String email,
+        String nickname
+) {
+
+
+    public static ArticleResponse of(Long id, String title, String content, String hashtags, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleResponse(id, title, content, hashtags, createdAt, email, nickname);
+    }
+
+    public static ArticleResponse from(ArticleDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return new ArticleResponse(
+                dto.id(),
+                dto.title(),
+                dto.content(),
+                dto.hashtag()
+//                dto.hashtagDtos().stream()
+//                        .map(HashtagDto::hashtagName)
+//                        .collect(Collectors.toUnmodifiableSet())
+                ,
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname
+        );
+    }
+
+}
